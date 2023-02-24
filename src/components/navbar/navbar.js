bde02b2e665329bf4e9fe1a80c6b93893242c745
Fanush <fgabriel.mihailescu@gmail.com>
2023-02-24 16:03:57 +0200

    added Link

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,6 +1,6 @@
 import styles from './navbar.module.scss';
 import MenuIcon from '@mui/icons-material/Menu';
-
+import { Link } from 'react-router-dom';
 export default function Navbar(){
 
     return(
@@ -8,10 +8,10 @@ export default function Navbar(){
         <div className={styles.navcontainer}>
             <h1>Dashboard</h1>
             <ul className={styles.nav__list}>
-                <li><a className={styles.nav__item} href="/">Home</a></li>
-                <li><a className={styles.nav__item} href="login">Login</a></li>
-                <li><a className={styles.nav__item} href="register">Register</a></li>
-                <li><a className={styles.nav__item} href="forgot-password">Forgot Password</a></li>
+                <li><Link className={styles.nav__item} to="/">Home</Link></li>
+                <li><Link className={styles.nav__item} to="/login">Login</Link></li>
+                <li><Link className={styles.nav__item} to="/register">Register</Link></li>
+                <li><Link className={styles.nav__item} to="/forgot-password">Forgot Password</Link></li>
             </ul>
             <MenuIcon className={styles.hamburger__icon}/>
         </div>
