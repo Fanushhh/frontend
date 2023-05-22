1bfa6240058d89daf352fc134ede79796512ae47
Mihailescu Fanush Gabriel <84035206+Fanushhh@users.noreply.github.com>
2023-05-22 15:33:27 +0300

    Update Exercise.js

--- src/components/exercise/Exercise.js
+++ src/components/exercise/Exercise.js
@@ -3,9 +3,24 @@ import styles from './Exercise.module.scss';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from "@mui/icons-material/Delete";
 import {Link} from 'react-router-dom';
+import axios from 'axios';
+
+
+
 
 export default function Exercise({exerciseData}){
-
+    
+  const deleteItem = async (exerciseData._id) => {
+    try {
+      await axios.delete(`/api/exercises/${itemId}`);
+      console.log('Item deleted successfully!');
+      // Perform any necessary state updates or UI changes
+    } catch (error) {
+      console.error(error);
+      // Handle the error, show a notification, etc.
+    }
+  };
+    
     return(
         <div className={styles.exercise}>
             <Link  to={`/${exerciseData._id}`}>
@@ -24,6 +39,7 @@ export default function Exercise({exerciseData}){
                     <button 
                     data-hover="Delete me" 
                     className={styles.deleteButton}
+                    onClick={deleteItem}
                     >
                     <DeleteIcon /> 
                     </button>
