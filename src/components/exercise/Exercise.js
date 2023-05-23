d53d2613e1e1aeb6c7a36746f45e7c9271fecdca
Mihailescu Fanush Gabriel <84035206+Fanushhh@users.noreply.github.com>
2023-05-23 07:47:05 +0300

    Update Exercise.js

--- src/components/exercise/Exercise.js
+++ src/components/exercise/Exercise.js
@@ -9,6 +9,7 @@ import axios from 'axios';
 
 
 export default function Exercise({exerciseData}){
+    
     const deleteItem = async (exerciseData) => {
     await fetch(`https://code-learn-v1.herokuapp.com/api/exercises/${exerciseData._id}`, {
     method: "DELETE",
