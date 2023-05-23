93ec4fc84b523756d663ff174859ba57c2c119bf
Fanush <fgabriel.mihailescu@gmail.com>
2023-05-23 11:23:47 +0300

    update on delete button

--- src/components/exercise/Exercise.js
+++ src/components/exercise/Exercise.js
@@ -2,34 +2,47 @@ import React from "react";
 import styles from "./Exercise.module.scss";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-export default function Exercise({exerciseData}){
+export default function Exercise({ exerciseData }) {
+  const id = exerciseData._id;
 
-    return(
-        <div className={styles.exercise}>
-            <Link  to={`/${exerciseData._id}`}>
-                <p className={styles.exerciseTitle}>
-                {exerciseData.name}
-                </p>
-            </Link>
-            <div className={styles.buttonsContainer}>
-                <div className={styles.buttonWrapper}>
-                    <button data-hover="Edit me" className={styles.editButton}>
-                    <EditIcon />
-                    </button>
-                    <span className={styles.editTooltip}>Edit me</span>
-                </div>
-                <div className={styles.buttonWrapper}>
-                    <button 
-                    data-hover="Delete me" 
-                    className={styles.deleteButton}
-                    >
-                    <DeleteIcon /> 
-                    </button>
-                    <span className={styles.deleteTooltip}>Delete me</span>
-                </div>
-            </div>
+  const deleteExercise = async () => {
+    try {
+      const response = await fetch(
+        `https://code-learn-v1.herokuapp.com/api/exercises/${id}`,
+        {
+          method: "DELETE",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log(response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className={styles.exercise}>
+      <Link to={`/${exerciseData._id}`}>
+        <p className={styles.exerciseTitle}>{exerciseData.name}</p>
+      </Link>
+      <div className={styles.buttonsContainer}>
+        <div className={styles.buttonWrapper}>
+          <button data-hover="Edit me" className={styles.editButton}>
+            <EditIcon />
+          </button>
+          <span className={styles.editTooltip}>Edit me</span>
         </div>
-    )
+        <div className={styles.buttonWrapper}>
+          <button onClick={deleteExercise} data-hover="Delete me" className={styles.deleteButton}>
+            <DeleteIcon />
+          </button>
+          <span className={styles.deleteTooltip}>Delete me</span>
+        </div>
+      </div>
+    </div>
+  );
 }
